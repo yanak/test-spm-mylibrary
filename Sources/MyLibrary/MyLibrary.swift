9af20fe49010db3e0c8d7de7eb020b1ac9453959
Yasunori Tanaka (田中 穏識) <tanaka.yasunori@icloud.com>
2024-11-27 17:30:43 +0900

    use SwiftUI

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,2 +1,18 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+@available(macOS 14.6, *)
+@available(iOS 17.0, *)
+public struct MyLibrary: View {
+    public init() {
+        
+    }
+    public var body: some View {
+        HStack {
+           Image(systemName: "star.fill")
+            Text("初めてのSPM")
+        }
+    }
+}
